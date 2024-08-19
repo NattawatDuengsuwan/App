c06d6799f9b47fcd8c90bdd3bfe7d532776be192
NattawatDuengsuwan <nattawatpapa13@gmail.com>
2024-08-19 10:49:28 +0700

    งานวันนี้
    งานวันนี้

--- App1/src/App.js
+++ App1/src/App.js
@@ -1,15 +1,14 @@
 import  React  from 'react';
+import RefsFunc from './refs-func'
 import './style.css'
-// import Calendar from './class-component'
+
 import {Header, Content, Footer} from './func-component'  
 
 function App(){
   return (
-    <>
-        <Header/>
-        <Content/>
-        <Footer/>
-    </>
+    
+        
+    <RefsFunc/>
 
   )
 }
